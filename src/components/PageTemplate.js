86cfb14e00a737c2f1706787ec0b306cd0fab2c2
kimseungwon <kimseungwon9297@gmail.com>
2018-09-04 11:56:25 +0900

    update

--- src/components/PageTemplate.js
+++ src/components/PageTemplate.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './PageTemplate';
+import styles from './PageTemplate.scss';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
